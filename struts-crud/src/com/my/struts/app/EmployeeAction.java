9baabca0781d39a95297db05745baee8fab6741a
yufeng218 <he1012315243@163.com>
2016-10-10 23:55:50 +0800

    使用ModelDriven接口传值

--- struts-crud/src/com/my/struts/app/EmployeeAction.java
+++ struts-crud/src/com/my/struts/app/EmployeeAction.java
@@ -4,44 +4,41 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.RequestAware;
 
-public class EmployeeAction implements RequestAware
+import com.opensymphony.xwork2.ModelDriven;
+
+public class EmployeeAction implements RequestAware, ModelDriven<Employee>
 {
 	private Dao dao = new Dao();
-	private Map<String, Object> request;
+
+	private Employee employee;
 
 	public String list()
 	{
 		request.put("emps", dao.getEmployees());
 		return "list";
 	}
-	
-	//定义的employeeId，用来接收页面的请求参数
-	private Integer employeeId;
-	
+
 	public String delete()
 	{
-		dao.delete(employeeId);
-		
-		//返回的结果类型为: redirectAction;
-		//也可以是chain: 实际上chain是没有必要的，因为不需要在下一个Action中保留当前Aciton的状态
-		//若使用chain，则到达目录页面后，地址栏显示的仍然是删除的那个链接，页面刷新时会有重复提交;
+		dao.delete(employee.getEmployeeId());
+
+		// 返回的结果类型为: redirectAction;
+		// 也可以是chain: 实际上chain是没有必要的，因为不需要在下一个Action中保留当前Aciton的状态
+		// 若使用chain，则到达目录页面后，地址栏显示的仍然是删除的那个链接，页面刷新时会有重复提交;
 		return "success";
 	}
-	
-	private String firstName;
-	private String lastName;
-	private String email;
-	
+
 	public String save()
 	{
-		//1、获取请求参数: 通过定义对应属性的方式
-		//2、调用 Dao 的 save 方法
-		Employee employee = new Employee(null, firstName, lastName, email);
+		// 1、获取请求参数: 通过定义对应属性的方式
+		// 2、调用 Dao 的 save 方法
 		dao.save(employee);
-		
-		//3、通过redirectAction 的方式响应结果给 emp-list
+
+		// 3、通过redirectAction 的方式响应结果给 emp-list
 		return "success";
 	}
+	
+	private Map<String, Object> request;
 	
 	@Override
 	public void setRequest(Map<String, Object> request)
@@ -49,44 +46,11 @@ public class EmployeeAction implements RequestAware
 		this.request = request;
 	}
 
-	public Integer getEmployeeId()
+	@Override
+	public Employee getModel()
 	{
-		return employeeId;
+		employee = new Employee();
+		return employee;
 	}
 
-	public void setEmployeeId(Integer employeeId)
-	{
-		this.employeeId = employeeId;
-	}
-
-	public String getFirstName()
-	{
-		return firstName;
-	}
-
-	public void setFirstName(String firstName)
-	{
-		this.firstName = firstName;
-	}
-
-	public String getLastName()
-	{
-		return lastName;
-	}
-
-	public void setLastName(String lastName)
-	{
-		this.lastName = lastName;
-	}
-
-	public String getEmail()
-	{
-		return email;
-	}
-
-	public void setEmail(String email)
-	{
-		this.email = email;
-	}
-	
 }
